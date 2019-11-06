f29c774bae453d8f1c05824fb7ccd0831e2712c5
Trevor Page <trevor@craftycodr.com>
2019-11-06 10:15:37 -0500

    Week2-2

--- Week2/src/com/coderscampus/week2/Week2Example.java
+++ Week2/src/com/coderscampus/week2/Week2Example.java
@@ -3,6 +3,14 @@ package com.coderscampus.week2;
 public class Week2Example {
 
 	public static void main (String[] args) {
-		System.out.println("Hello World");
+		int age = 19;
+		
+		if (age >= 19) {       
+			System.out.println("You are an adult");
+		} else {
+			System.out.println("You are NOT an adult");
+		}
+		
+		System.out.println("Your age is " + age);
 	}
 }

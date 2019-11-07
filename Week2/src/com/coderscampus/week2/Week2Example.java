059d9ef70de428e34650b11ccc8aaf49e8061260
Trevor Page <trevor@craftycodr.com>
2019-11-07 10:47:38 -0500

    Week2-5

--- Week2/src/com/coderscampus/week2/Week2Example.java
+++ Week2/src/com/coderscampus/week2/Week2Example.java
@@ -1,31 +1,22 @@
 package com.coderscampus.week2;
 
+import java.util.Scanner;
+
 public class Week2Example {
 
 	public static void main (String[] args) {
-		int age = 21;
 		
-		if (age >= 19) {       
-			System.out.println("You are an adult");
-			if (age > 65) {
-				System.out.println("You are a senior");
-			}
-		} else if (age >= 13) {
-			System.out.println("You are a teenager");
-		} else if (age == 12) {
-			System.out.println("You are a pre-teen");
-		} else {
-			System.out.println("You are a child");
-		}
+		// This is just for teaching purposes ///////
+		//  with respect to imports / packages     //
+		// java.lang is by default imported for us //
+		java.lang.String aString = "Hello World";  //
+		String anotherString = "Another String";   //
+		/////////////////////////////////////////////
 		
-		String output = "Your age is " + age;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Type in any input:");
+		String userInput = scanner.nextLine();
 		
-		System.out.println(output);
-		
-		if (output.equals("Your age is 18")) {
-			System.out.println("Yay you can vote!");
-		} else if (output.equals("Your age is 21")) {
-			System.out.println("Yay you can drink / gamble!");
-		}
+		System.out.println("You typed in: " + userInput);
 	}
 }

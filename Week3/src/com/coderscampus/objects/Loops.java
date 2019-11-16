767886cb1971d5ed6d55f1326157ead4e7862e9d
Trevor Page <trevor@craftycodr.com>
2019-11-16 15:55:24 -0500

    Week 3-5

---
+++ Week3/src/com/coderscampus/objects/Loops.java
@@ -0,0 +1,19 @@
+package com.coderscampus.objects;
+
+public class Loops {
+	
+	public static void main (String[] args) {
+		
+		// init; termination condition; increment
+		for 
+		  (int i = 0; 
+				i <= 100; 
+				i = i + 1) 
+		{
+			System.out.print(i + ": ");
+			System.out.println(i*i);
+		}
+		
+		System.out.println("Done");
+	}
+}

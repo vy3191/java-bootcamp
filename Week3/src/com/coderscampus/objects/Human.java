e89e688005bf460f1f0f15062c03b2a4ba949d5d
Trevor Page <trevor@craftycodr.com>
2019-11-17 19:06:46 -0500

    weekWeek 3-9

--- Week3/src/com/coderscampus/objects/Human.java
+++ Week3/src/com/coderscampus/objects/Human.java
@@ -6,17 +6,21 @@ public class Human {
 	String weight;
 	String eyeColor;
 	
+	Human () {
+		System.out.println("A human is being created");
+	}
+	
+	Human (String gender, String height, String weight, String eyeColor) {
+		this.gender = gender;
+		this.height = height;
+		this.weight = weight;
+		this.eyeColor = eyeColor;
+	}
+	
 	// (visibility modifier), return type, method name, params, scope
 	String getInfo () {
 		return "Gender: " + gender + ", Height: " + height + 
 			   ", Eye Color: " + eyeColor + ", Weight: " + weight;
 	}
 	
-	void setProperties (String gender, String height, 
-			String weight, String eyeColor) {
-		this.gender = gender;
-		this.height = height;
-		this.weight = weight;
-		this.eyeColor = eyeColor;
-	}
 }
--- Week3/src/com/coderscampus/objects/RunnableClass.java
+++ Week3/src/com/coderscampus/objects/RunnableClass.java
@@ -1,5 +1,7 @@
 package com.coderscampus.objects;
 
+import java.util.Random;
+
 public class RunnableClass {
 
 	public static void main(String[] args) {
@@ -7,26 +9,25 @@ public class RunnableClass {
 		// Object -> a Noun, it's an instantiation of a Class
 		
 		// Data type, variable name, assignment operation
-		Random random = new Random();
+//		Random random = new Random();
+//		
+//		for (int i = 0; i<20; i = i + 1) {
+//			System.out.println("Random number is " + random.nextInt(10)); 
+//		}
 		
-		Human trevorPage = new Human();
-		for (int i = 0; i<20; i = i + 1) {
-			System.out.println("Random number is " + random.nextInt(10)); 
-		}
+		// Call the no arg constructor
+		Human someOtherHuman = new Human();
 		
-		trevorPage.setProperties("male", "5'10\"", "160", "brown");
+		// call the 4 arg constructor
+		Human trevorPage = new Human("male", "5'10\"", "160", "brown");
 		
 		System.out.println(trevorPage.getInfo());
 		
-		Human elonMusk = new Human();
-		
-		elonMusk.setProperties("male", "6'1\"", "190", "blue");
+		Human elonMusk = new Human("male", "6'1\"", "190", "blue");
 		
 		System.out.println(elonMusk.getInfo());
 		
-		Human christinaPage = new Human();
-		
-		christinaPage.setProperties("female", "5'5\"", "130", "brown");
+		Human christinaPage = new Human("female", "5'5\"", "130", "brown");
 		
 		System.out.println(christinaPage.getInfo());
 	}

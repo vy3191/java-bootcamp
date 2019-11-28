4f18746bb86eadf35cafacf19cd627faa9eb02a4
Trevor Page <trevor@craftycodr.com>
2019-11-28 10:19:32 -0500

    week5-09

---
+++ Week5/src/com/coderscampus/lesson8/ExceptionsExamples.java
@@ -0,0 +1,18 @@
+package com.coderscampus.lesson8;
+
+public class ExceptionsExamples {
+
+	public static void main (String[] args) {
+		
+		String someString = null;
+		
+		try {
+			String[] stringArray = someString.split(",");
+			System.out.println(stringArray);
+		} catch (NullPointerException e) {
+			System.out.println("Oops, there was a null pointer exception");
+		}
+		
+		
+	}
+}

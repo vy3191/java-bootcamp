da757ec0de5e3209ca5edd6c978024b01ae9cf60
Trevor Page <trevor@craftycodr.com>
2019-12-03 08:54:16 -0500

    03 - Extending an Abstract Class

---
+++ Week7/src/com/coderscampus/lesson2/CarFactory.java
@@ -0,0 +1,11 @@
+package com.coderscampus.lesson2;
+
+public class CarFactory extends GenericFactory {
+
+	@Override
+	public void build(Part[] parts) {
+		
+		
+	}
+
+}
---
+++ Week7/src/com/coderscampus/lesson2/Part.java
@@ -0,0 +1,6 @@
+package com.coderscampus.lesson2;
+
+public interface Part {
+	Integer getSize();
+	Integer getBuildTime();
+}

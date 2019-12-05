07ec67489b3dd80542b21771ec2ecee2bcda3a15
Trevor Page <trevor@craftycodr.com>
2019-12-05 15:52:08 -0500

    08 - RegEx Part II

--- Week7/src/com/coderscampus/lesson7/RegExApplication.java
+++ Week7/src/com/coderscampus/lesson7/RegExApplication.java
@@ -10,10 +10,24 @@ public class RegExApplication {
 		// valid email address: trevor-page.2@gmail.com
 		// valid email address: Trevor@CraftyCodr.com
 		
-		String input = "fake.email@gmail.com";
-		boolean matchFound = input.matches("[a-z]+.?[a-z]+@[a-z]+.[a-z]{1,5}");
-		
-		System.out.println(matchFound);
+		// valid
+		inputMatchesRegExPattern("fake.email@gmail.com");
+		// invalid (missing an @ symbol)
+		inputMatchesRegExPattern("trevorcraftycodr.com");
+		// invalid (domain extension is too long)
+		inputMatchesRegExPattern("trevor@craftycodr.thisisntarealdomain");
+		// valid
+		inputMatchesRegExPattern("trevor@craftycodr.com");
+		// valid
+		inputMatchesRegExPattern("trevor-page.2@gmail.com");
+		// valid
+		inputMatchesRegExPattern("Trevor@CraftyCodr.com");
+	}
+
+	private static void inputMatchesRegExPattern(String input) {
+		boolean matchFound = input.matches("^[a-zA-Z0-9][a-zA-Z0-9-]{0,}\\.?[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]{1,5}$");
+		System.out.println("For Input " + input + ", match found = " + matchFound);
 	}
 
 }
+

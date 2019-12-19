95063299f84539acc174e8059d09b5f78492bc8f
Trevor Page <trevor@craftycodr.com>
2019-12-19 10:09:52 -0500

    02 - ArrayList

--- Week9/src/ListApplication.java
+++ Week9/src/ListApplication.java
@@ -12,15 +12,38 @@ public class ListApplication {
 		
 		names[1] = null;
 		
+		System.out.println("Arrays example: ");
 		for (String name : names) {
 			System.out.println(name);
 		}
-		
+		System.out.println("-------------");
+		System.out.println("");
 		// Collections -> Java solution to common data structure problems
 		// List (a type of Collection)
 		// Set (a type of Collection)
 		// Map (a type of Collection)
 		
 		List<String> elements = new ArrayList<String>();
+		elements.add("Trevor Page");
+		elements.add("John Doe");
+		elements.add("Jane Doe");
+		elements.add("Elon Musk");
+		elements.add("Josh Someone");
+		
+		System.out.println("List example: ");
+		printArrayListToConsole(elements);
+		
+		elements.remove(1);
+		printArrayListToConsole(elements);
+		
+		elements.remove("Josh Someone");
+		printArrayListToConsole(elements);
+	}
+
+	private static void printArrayListToConsole(List<String> elements) {
+		for (String element : elements) {
+			System.out.println(element);
+		}
+		System.out.println("");
 	}
 }

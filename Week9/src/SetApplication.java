798b711eb64b5b60f0816841b0b4d3ecc6a5257e
Trevor Page <trevor@craftycodr.com>
2019-12-20 09:08:04 -0500

    04 - Sets

---
+++ Week9/src/SetApplication.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetApplication {
+  public static void main(String[] args) {
+	Set<Integer> uniqueNumbers = new LinkedHashSet<>();
+	
+	System.out.println(uniqueNumbers.add(1));
+	System.out.println(uniqueNumbers.add(2));
+	System.out.println(uniqueNumbers.add(3));
+	System.out.println(uniqueNumbers.add(4));
+	System.out.println(uniqueNumbers.add(5));
+	System.out.println(uniqueNumbers.add(6));
+	System.out.println(uniqueNumbers.add(1));
+	
+	printSetToConsole(uniqueNumbers);
+	
+	Set<String> uniqueNames = new HashSet<>();
+	uniqueNames.add("Trevor Page");
+	uniqueNames.add("Elon Musk");
+	uniqueNames.add("Jane Doe");
+	uniqueNames.add("Josh Someone");
+	
+	for (String element : uniqueNames) {
+		System.out.println(element);
+	}
+  }
+  
+  private static void printSetToConsole(Set<Integer> elements) {
+	for (Integer element : elements) {
+		System.out.println(element);
+	}
+	System.out.println("");
+  }
+}

c0163fbbb6d6963fb29f68fadc01921ddd319d44
Trevor Page <trevor@craftycodr.com>
2019-12-21 10:18:42 -0500

    07 - HashCode and Equals

---
+++ Week9/src/Person.java
@@ -0,0 +1,60 @@
+
+public class Person {
+	private Integer ssn;
+	private String firstName;
+	private String lastName;
+	
+	public Person(int ssn, String firstName, String lastName) {
+		this.ssn = ssn;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public Integer getSsn() {
+		return ssn;
+	}
+	public void setSsn(Integer ssn) {
+		this.ssn = ssn;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	@Override
+	public String toString() {
+		return "Person [ssn=" + ssn + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ssn == null) ? 0 : ssn.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (ssn == null) {
+			if (other.ssn != null)
+				return false;
+		} else if (!ssn.equals(other.ssn))
+			return false;
+		return true;
+	}
+}
--- Week9/src/SetApplication.java
+++ Week9/src/SetApplication.java
@@ -3,34 +3,48 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetApplication {
-  public static void main(String[] args) {
-	Set<Integer> uniqueNumbers = new LinkedHashSet<>();
-	
-	System.out.println(uniqueNumbers.add(1));
-	System.out.println(uniqueNumbers.add(2));
-	System.out.println(uniqueNumbers.add(3));
-	System.out.println(uniqueNumbers.add(4));
-	System.out.println(uniqueNumbers.add(5));
-	System.out.println(uniqueNumbers.add(6));
-	System.out.println(uniqueNumbers.add(1));
-	
-	printSetToConsole(uniqueNumbers);
-	
-	Set<String> uniqueNames = new HashSet<>();
-	uniqueNames.add("Trevor Page");
-	uniqueNames.add("Elon Musk");
-	uniqueNames.add("Jane Doe");
-	uniqueNames.add("Josh Someone");
-	
-	for (String element : uniqueNames) {
-		System.out.println(element);
+	public static void main(String[] args) {
+		Set<Person> people = new HashSet<>();
+
+		people.add(new Person(111111111, "Trevor", "Page"));
+		people.add(new Person(111111111, "Trevor", "Page"));
+		people.add(new Person(111111112, "Trevor", "Page"));
+
+		for (Person person : people) {
+			System.out.println(person);
+		}
+
+//	  example1();
 	}
-  }
-  
-  private static void printSetToConsole(Set<Integer> elements) {
-	for (Integer element : elements) {
-		System.out.println(element);
+
+	private static void example1() {
+		Set<Integer> uniqueNumbers = new LinkedHashSet<>();
+
+		System.out.println(uniqueNumbers.add(1));
+		System.out.println(uniqueNumbers.add(2));
+		System.out.println(uniqueNumbers.add(3));
+		System.out.println(uniqueNumbers.add(4));
+		System.out.println(uniqueNumbers.add(5));
+		System.out.println(uniqueNumbers.add(6));
+		System.out.println(uniqueNumbers.add(1));
+
+		printSetToConsole(uniqueNumbers);
+
+		Set<String> uniqueNames = new HashSet<>();
+		uniqueNames.add("Trevor Page");
+		uniqueNames.add("Elon Musk");
+		uniqueNames.add("Jane Doe");
+		uniqueNames.add("Josh Someone");
+
+		for (String element : uniqueNames) {
+			System.out.println(element);
+		}
 	}
-	System.out.println("");
-  }
+
+	private static void printSetToConsole(Set<Integer> elements) {
+		for (Integer element : elements) {
+			System.out.println(element);
+		}
+		System.out.println("");
+	}
 }

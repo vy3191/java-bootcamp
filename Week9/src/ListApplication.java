f02e1d8804a4da7dfadfa7c7584a07ce40c71087
Trevor Page <trevor@craftycodr.com>
2019-12-21 11:18:05 -0500

    Revert "08 - Lambdas"
    This reverts commit 438ef7abc1c131f4216227ddd1b6b34bc1baaa50.

--- Week9/src/ListApplication.java
+++ Week9/src/ListApplication.java
@@ -1,45 +1,8 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class ListApplication {
 	public static void main(String[] args) {
-		
-		List<String> names = new ArrayList<String>();
-		names.add("Trevor Page");
-		names.add("John Doe");
-		names.add("Jane Doe");
-		names.add("Elon Musk");
-		names.add("Josh Someone");
-		
-		Collections.sort(names, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				return o2.compareTo(o1);
-			}
-		});
-		
-		Collections.sort(names, (o1, o2) -> o2.compareTo(o1));
-		
-		Collections.sort(names, (o1, o2) -> { 
-			  return o2.compareTo(o1); 
-			}
-		);
-		
-		Collections.sort(names, (o1, o2) -> {
-		  if (o2 != null && o1 != null) {
-			return o2.compareTo(o1);
-		  }
-		  return 0;
-		}); 
-		
-		printArrayListToConsole(names);
-		
-//		example1();
-	}
-
-	private static void example1() {
 		String[] names = new String[4];
 		
 		names[0] = "Trevor Page";

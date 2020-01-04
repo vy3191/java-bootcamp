45eadf330dffd4a8676cbe9235464ecbed23a7b8
Trevor Page <trevor@craftycodr.com>
2020-01-04 15:49:42 -0500

    07 - Modifying Java Util Dates

--- Week10/src/com/coderscampus/lesson6/DatesApplication.java
+++ Week10/src/com/coderscampus/lesson6/DatesApplication.java
@@ -1,9 +1,38 @@
 package com.coderscampus.lesson6;
 
+import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class DatesApplication {
 	public static void main(String[] args) {
+//		lesson6();
+		
+		// This is deprecated, don't use this approach, use Calendar instead
+		Date jan012020 = new Date(120, 0, 1, 0, 0, 0);
+		Calendar cal = Calendar.getInstance();
+		System.out.println(jan012020);
+		
+		Date someOtherDate = cal.getTime();
+		cal.set(2020, 0, 1, 15, 1, 5);
+		someOtherDate = cal.getTime();
+		System.out.println(someOtherDate);
+		
+		cal.add(Calendar.HOUR, 2);
+		System.out.println(cal.getTime());
+		
+		cal.add(Calendar.MINUTE, -1);
+		System.out.println(cal.getTime());
+		
+		Date now = new Date();
+		if (now.after(cal.getTime())) {
+			System.out.println(now + " is after " + cal.getTime());
+		}
+		
+	}
+
+	private static void lesson6() {
 		Date now = new Date();
 		System.out.println(now);
 		

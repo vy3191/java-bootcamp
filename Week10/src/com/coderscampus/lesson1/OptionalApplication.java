b1a2460f8204b463fbd1c721dfe389c6da0d5fb9
Trevor Page <trevor@craftycodr.com>
2020-01-04 11:34:21 -0500

    03 - Optional Map OrElse

--- Week10/src/com/coderscampus/lesson1/OptionalApplication.java
+++ Week10/src/com/coderscampus/lesson1/OptionalApplication.java
@@ -1,5 +1,6 @@
 package com.coderscampus.lesson1;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 public class OptionalApplication {
@@ -14,7 +15,21 @@ public class OptionalApplication {
 	Recipe cookies = new Recipe();
 	cookies.setName("Chocolate Chip Cookies");
 	cookies.setInstructions("1. Pre-heat oven\n2. Put dough on baking sheet\n3. Cook dough for 15 minutes");
+//	cookies.setPriceOpt(new BigDecimal(9.99));
 	
 	System.out.println(cookies);
+	
+	// Good way to get the wrapped data in Optional
+	BigDecimal price = cookies.getPriceOpt()
+							  .map(p -> p)
+							  .orElse(new BigDecimal(0));
+	
+	// "Bad" way to get the wrapped data in Optional
+//	if (cookies.getPriceOpt().isPresent()) {
+//		BigDecimal price = cookies.getPriceOpt().get();
+//		System.out.println(price);
+//	}
+	
+	
   }
 }

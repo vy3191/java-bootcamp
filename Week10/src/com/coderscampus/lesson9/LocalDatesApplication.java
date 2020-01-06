42e6bc432b133701b0a373c330d1d17c0000aa84
Trevor Page <trevor@craftycodr.com>
2020-01-06 13:44:53 -0500

    11 - Parsing Java 8 Dates

--- Week10/src/com/coderscampus/lesson9/LocalDatesApplication.java
+++ Week10/src/com/coderscampus/lesson9/LocalDatesApplication.java
@@ -5,12 +5,26 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 
 public class LocalDatesApplication {
 	public static void main(String[] args) {
 //		lesson9();
-		
+//		lesson10();
+		try {
+			LocalDateTime jan1 = LocalDateTime.parse("2020-01-01 3:00 PM", DateTimeFormatter.ofPattern("yyyy-MM-dd h:mm a"));
+			System.out.println(jan1);
+			
+			LocalDate endOfFeb = LocalDate.of(2020, 02, 5).with(TemporalAdjusters.lastDayOfMonth());
+			System.out.println(endOfFeb);
+		} catch (DateTimeParseException e) {
+			System.out.println("Oops it doesn't look like your Date/Time is formatted correctly");
+		}
+	}
+
+	private static void lesson10() {
 		LocalDateTime jan1 = LocalDateTime.of(2020, 1, 1, 13, 30, 0);
 		System.out.println(jan1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd h:mm:ss a")));
 		
@@ -22,7 +36,6 @@ public class LocalDatesApplication {
 		
 		jan3 = jan3.minus(1, ChronoUnit.DAYS);
 		System.out.println(jan3);
-		
 	}
 
 	private static void lesson9() {

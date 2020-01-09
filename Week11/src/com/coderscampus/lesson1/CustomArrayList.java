c269563a7735d5e743434509592e5d2585178dc4
Trevor Page <trevor@craftycodr.com>
2020-01-09 15:44:46 -0500

    01 - What is Unit Testing

---
+++ Week11/src/com/coderscampus/lesson1/CustomArrayList.java
@@ -0,0 +1,24 @@
+package com.coderscampus.lesson1;
+
+public class CustomArrayList<T> implements CustomList<T> {
+	Object[] items = new Object[10];
+
+	@Override
+	public boolean add(T item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}

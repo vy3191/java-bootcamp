b36e5fb64bf834527fd0e7edfee22df2eb6a4c11
Trevor Page <trevor@craftycodr.com>
2020-01-10 12:20:23 -0500

    02 - Test Driven Development

--- Week11/src/com/coderscampus/lesson1/CustomArrayList.java
+++ Week11/src/com/coderscampus/lesson1/CustomArrayList.java
@@ -2,23 +2,23 @@ package com.coderscampus.lesson1;
 
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
-
+	int size = 0;
+	
 	@Override
 	public boolean add(T item) {
-		// TODO Auto-generated method stub
-		return false;
+		items[size++] = item;
+		return true;
 	}
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public T get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return (T) items[index]; 
 	}
 	
 }
--- Week11/test/com/coderscampus/lesson1/CustomArrayListTest.java
+++ Week11/test/com/coderscampus/lesson1/CustomArrayListTest.java
@@ -5,10 +5,31 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class CustomArrayListTest {
+	// Test-Driven Development Methodology
+	// Step 1 - Write a failing test
+	// Step 2 - Write the business logic to make the test pass  
+	// Step 3 - Refactor your code
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void should_add_one_item_to_list() {
+		// Three A's
+		// Arrange, Act, Assert
+		
+		// Arrange
+		CustomList<Integer> customList = new CustomArrayList<>();
+		
+		// Act
+		customList.add(10);
+		Integer expectedResult = customList.get(0);
+		Integer expectedSize = customList.getSize();
+		
+		// Assert
+		// What would we expect to happen?
+		//  I expect there to be an Integer with value 10 stored in the first
+		//  index in my customList
+		assertEquals(10, expectedResult);
+		assertEquals(1, expectedSize);
+		
 	}
 
 }

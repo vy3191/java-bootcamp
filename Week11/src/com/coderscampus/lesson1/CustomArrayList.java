f57e472f4927152640a5b99d2230e6a9cdafd639
Trevor Page <trevor@craftycodr.com>
2020-01-10 14:09:55 -0500

    03 - More Unit Test Examples

--- Week11/src/com/coderscampus/lesson1/CustomArrayList.java
+++ Week11/src/com/coderscampus/lesson1/CustomArrayList.java
@@ -6,8 +6,19 @@ public class CustomArrayList<T> implements CustomList<T> {
 	
 	@Override
 	public boolean add(T item) {
+		if (items.length == size) {
+			expandBackingObjectArray();
+		}
 		items[size++] = item;
 		return true;
+	}
+
+	private void expandBackingObjectArray() {
+		Object[] oldArray = items;
+		items = new Object[size * 2];
+		for (int i=0; i<oldArray.length; i++) {
+			items[i] = oldArray[i];
+		}
 	}
 
 	@Override
--- Week11/test/com/coderscampus/lesson1/CustomArrayListTest.java
+++ Week11/test/com/coderscampus/lesson1/CustomArrayListTest.java
@@ -16,12 +16,12 @@ class CustomArrayListTest {
 		// Arrange, Act, Assert
 		
 		// Arrange
-		CustomList<Integer> customList = new CustomArrayList<>();
+		CustomList<Integer> sut = new CustomArrayList<>();
 		
 		// Act
-		customList.add(10);
-		Integer expectedResult = customList.get(0);
-		Integer expectedSize = customList.getSize();
+		sut.add(10);
+		Integer expectedResult = sut.get(0);
+		Integer expectedSize = sut.getSize();
 		
 		// Assert
 		// What would we expect to happen?
@@ -29,7 +29,39 @@ class CustomArrayListTest {
 		//  index in my customList
 		assertEquals(10, expectedResult);
 		assertEquals(1, expectedSize);
-		
 	}
-
+	
+	@Test
+	void should_add_11_items_to_list () {
+		// Arrange
+		CustomList<Integer> sut = new CustomArrayList<>();
+		
+		// Act
+		sut.add(1); sut.add(2); sut.add(3); sut.add(4);
+		sut.add(5); sut.add(6); sut.add(7); sut.add(8);
+		sut.add(9); sut.add(10); sut.add(11);
+		
+		// Assert
+		for (int i=0; i<10; i++) {
+			assertEquals(i+1, sut.get(i));
+		}
+		assertEquals(11, sut.getSize());
+	}
+	
+	@Test
+	void should_add_100000_items_to_list () {
+		// Arrange
+		CustomList<Integer> sut = new CustomArrayList<>();
+		
+		// Act
+		for (int i=1; i<=100000; i++) {
+			sut.add(i);
+		}
+		
+		// Assert
+		for (int i=0; i<99999; i++) {
+			assertEquals(i+1, sut.get(i));
+		}
+		assertEquals(100000, sut.getSize());
+	}
 }

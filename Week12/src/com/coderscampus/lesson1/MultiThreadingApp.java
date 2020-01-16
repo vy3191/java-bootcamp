722ac71b9a92fee58f5e2684ffc572163e76f3f3
Trevor Page <trevor@craftycodr.com>
2020-01-16 16:45:33 -0500

    03 - Understanding Asynchronous Programming

--- Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
+++ Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
@@ -2,11 +2,17 @@ package com.coderscampus.lesson1;
 
 public class MultiThreadingApp {
 	public static void main(String[] args) {
+		String message = "Starting";
+		System.out.println(message);
+		System.out.println("Thead-"+ Thread.currentThread().getName());
 		
-		for (int i=0; i<5; i++) {
+		for (int i=0; i<50; i++) {
 			SomeTask task = new SomeTask();
 			new Thread(task).start();
 		}
+		
+		message = "Done";
+		System.out.println(message);
 	}
 	
 	
--- Week12/src/com/coderscampus/lesson1/SomeTask.java
+++ Week12/src/com/coderscampus/lesson1/SomeTask.java
@@ -1,9 +1,16 @@
 package com.coderscampus.lesson1;
 
+import java.util.Random;
+
 public class SomeTask implements Runnable {
 
 	@Override
 	public void run() {
+		for (int i=0; i<10000000; i++) {
+			Integer int1 = new Random().nextInt();
+			Integer int2 = new Random().nextInt();
+			Double something = (int1 + int2) * 1.5;
+		}
 		System.out.println("Hey look at me, I'm a task! "
 				+ "I'm running on thread: " + Thread.currentThread().getName());
 	}

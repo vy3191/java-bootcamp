f0846715c6284b484788b9a2eb8ea053358d531e
Trevor Page <trevor@craftycodr.com>
2020-01-20 14:10:29 -0500

    04 - Leveraging Executors

--- Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
+++ Week12/src/com/coderscampus/lesson1/MultiThreadingApp.java
@@ -1,14 +1,29 @@
 package com.coderscampus.lesson1;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MultiThreadingApp {
 	public static void main(String[] args) {
 		String message = "Starting";
 		System.out.println(message);
 		System.out.println("Thead-"+ Thread.currentThread().getName());
 		
+		// This starts up as many threads as there are iterations in the for loop.
+		// You only want to fire up a ton of threads, if those threads are going to
+		// be waiting a lot... i.e. sending requests across the internet.
+//		for (int i=0; i<50; i++) {
+//			SomeTask task = new SomeTask();
+//			new Thread(task).start();
+//		}
+		
+		// For a CPU bound operation like we have in our "SomeTask" class,
+		//  we should make use of an ExecutorService
+		
+		ExecutorService service = Executors.newFixedThreadPool(6);
+		
 		for (int i=0; i<50; i++) {
-			SomeTask task = new SomeTask();
-			new Thread(task).start();
+			service.execute(new SomeTask());
 		}
 		
 		message = "Done";
--- Week12/src/com/coderscampus/lesson1/SomeTask.java
+++ Week12/src/com/coderscampus/lesson1/SomeTask.java
@@ -3,7 +3,8 @@ package com.coderscampus.lesson1;
 import java.util.Random;
 
 public class SomeTask implements Runnable {
-
+	private static int taskId = 0;
+	
 	@Override
 	public void run() {
 		for (int i=0; i<10000000; i++) {
@@ -11,7 +12,8 @@ public class SomeTask implements Runnable {
 			Integer int2 = new Random().nextInt();
 			Double something = (int1 + int2) * 1.5;
 		}
-		System.out.println("Hey look at me, I'm a task! "
+		taskId++;
+		System.out.println("Hey look at me, I'm task-id: " + taskId + "! "
 				+ "I'm running on thread: " + Thread.currentThread().getName());
 	}
 	

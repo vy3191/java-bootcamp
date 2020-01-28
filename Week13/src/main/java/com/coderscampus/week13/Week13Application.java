59e16f0ec2575a7bd5180d60eb0e1fcb627cf2ac
Trevor Page <trevor@craftycodr.com>
2020-01-28 13:18:24 -0500

    01 - Small Introduction to Maven and Spring

---
+++ Week13/src/main/java/com/coderscampus/week13/Week13Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.week13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week13Application.class, args);
+	}
+
+}
---
+++ Week13/src/test/java/com/coderscampus/week13/Week13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.week13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c51c34761553c2e08954f51be0f82b136743ecd5
Trevor Page <trevor@craftycodr.com>
2020-01-29 16:15:02 -0500

    04 - Application Context and Singletons

--- Week13/src/main/java/com/coderscampus/week13/web/FileController.java
+++ Week13/src/main/java/com/coderscampus/week13/web/FileController.java
@@ -2,8 +2,10 @@ package com.coderscampus.week13.web;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +16,33 @@ import com.coderscampus.week13.service.FileService;
 public class FileController {
 	
 	@Autowired
-	private FileService fileService;
+	private ApplicationContext applicationContext;
 	
+	@GetMapping("/customer-report")
+	public Integer getCustomerData (Integer customerId) {
+		Integer customerData = 0;
+		// fetch data based on customerId
+		customerData = getCustomerDataById(customerId);
+		return customerData;
+	}
+	
+	private Integer getCustomerDataById(Integer customerId) {
+		return new Random().nextInt();
+	}
+
 	@GetMapping("/read-lines")
 	public List<String> readLines () throws IOException {
+		FileService fileService = applicationContext.getBean(FileService.class);
+		System.out.println(fileService);
+		fileService = applicationContext.getBean(FileService.class);
+		System.out.println(fileService);
+		fileService = applicationContext.getBean(FileService.class);
+		System.out.println(fileService);
+		fileService = applicationContext.getBean(FileService.class);
+		System.out.println(fileService);
+		fileService = applicationContext.getBean(FileService.class);
+		System.out.println(fileService);
+		
 		return fileService.readFile("test.txt");
 	}
 	

8e2e0a626966b40ef90c57ba7500d98849dd01b3
Trevor Page <trevor@craftycodr.com>
2020-01-30 09:08:12 -0500

    07 - Component Scanning

--- Week13/src/main/java/com/coderscampus/Week13Configuration.java
+++ Week13/src/main/java/com/coderscampus/Week13Configuration.java
@@ -1,4 +1,4 @@
-package com.coderscampus.week13;
+package com.coderscampus;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- Week13/src/main/java/com/coderscampus/week13/Week13Application.java
+++ Week13/src/main/java/com/coderscampus/week13/Week13Application.java
@@ -2,8 +2,10 @@ package com.coderscampus.week13;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.coderscampus")
 public class Week13Application {
 	
 	public static void main(String[] args) {

e438f589a05e19a5319d56182f2bccb3c87e04cd
Trevor Page <trevor@craftycodr.com>
2020-01-31 16:08:45 -0500

    12 - Importing a Maven Dependency

--- Week13/src/main/java/com/coderscampus/week13/Week13Application.java
+++ Week13/src/main/java/com/coderscampus/week13/Week13Application.java
@@ -1,5 +1,12 @@
 package com.coderscampus.week13;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,8 +15,20 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.coderscampus")
 public class Week13Application {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Week13Application.class, args);
+		
+		// example of how to parse a CSV file 
+		Reader in = new FileReader("recipes.txt");
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT
+											   .withIgnoreSurroundingSpaces()
+											   .withHeader()
+											   .withSkipHeaderRecord()
+											   .parse(in);
+		for (CSVRecord record : records) {
+			int cookingMinutes = Integer.parseInt(record.get("Cooking Minutes"));
+			boolean dairyFree = Boolean.parseBoolean(record.get("Dairy Free"));
+		}
 	}
 
 }

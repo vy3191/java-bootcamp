08d27b148a1e04874dff81b80782dbf6828ffb2c
Trevor Page <trevor@craftycodr.com>
2020-02-02 14:34:28 -0500

    01 - JSON

---
+++ Week14/src/main/java/com/coderscampus/Week14/JsonExample.java
@@ -0,0 +1,58 @@
+package com.coderscampus.Week14;
+
+public class JsonExample {
+
+	/**
+	 * JSON
+	 * JavaScript Object Notation
+	 * A simple String based way to notate data
+	 *  - Key : Value pairs
+	 *  - Key : Value pairs are separated by commas
+	 *  - Objects are represented by { }
+	 *  - Arrays are represented by [ ]
+	 *  
+	 * Important note: JSON is NOT tied to JavaScript... it is 
+	 *   language agnostic (meaning it'll work in any language)
+	 *   
+	 *   Person: name, age, gender
+	 *   
+	 *   {
+	 *   	"name" : "Trevor Page",
+	 *   	"age" : 37,
+	 *   	"gender" : "Male"
+	 *   }
+	 *   
+	 *   List<Person> people
+	 *   
+	 *   [
+	 *   	{
+	 *   		"name" : "Trevor Page",
+	 *   		"age" : 37,
+	 *   		"gender" : "Male"
+	 *   	},
+	 *   	{
+	 *   		"name" : "Jane Doe",
+	 *   		"age" : 29,
+	 *   		"gender" : "Female"
+	 *   	}
+	 *   ]
+	 *   
+	 *   
+	 *   XML
+	 *   
+	 *   <people>
+	 *   	<person>
+	 *   		<name>Trevor Page</name>
+	 *   		<age>37</age>
+	 *   		<gender>Male</gender>
+	 *   	</person>
+	 *		<person>
+	 *   		<name>Jane Doe</name>
+	 *   		<age>29</age>
+	 *   		<gender>Female</gender>
+	 *   	</person>	    
+	 *   </people>
+	 *   
+	 */
+	
+}
---
+++ Week14/src/main/java/com/coderscampus/Week14/Week14Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Week14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week14Application.class, args);
+	}
+
+}

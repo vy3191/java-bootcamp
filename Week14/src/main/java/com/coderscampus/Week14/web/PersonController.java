791669b8980b898f922eb26a96330920cec2a18b
Trevor Page <trevor@craftycodr.com>
2020-02-05 14:33:29 -0500

    06 - Path Variables

--- Week14/src/main/java/com/coderscampus/Week14/web/PersonController.java
+++ Week14/src/main/java/com/coderscampus/Week14/web/PersonController.java
@@ -1,5 +1,7 @@
 package com.coderscampus.Week14.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,15 @@ public class PersonController {
 		person.setAge(age);
 		person.setGender(gender);
 		return person;
+	}
+	
+	// fetches the person with ID 1
+	// http://localhost:8080/persons/1
+	
+	@GetMapping("/persons/{personId}")
+	public Person getPerson (@PathVariable Long personId) {
+		System.out.println("Person ID is: " + personId);
+		return null;
 	}
 	
 }

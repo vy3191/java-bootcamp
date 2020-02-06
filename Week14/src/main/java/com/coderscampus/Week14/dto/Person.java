cbf4870d1d94abe42f1536597dee494de3a8f649
Trevor Page <trevor@craftycodr.com>
2020-02-06 08:49:08 -0500

    07 - Mocking a Repository

--- Week14/src/main/java/com/coderscampus/Week14/dto/Person.java
+++ Week14/src/main/java/com/coderscampus/Week14/dto/Person.java
@@ -1,10 +1,17 @@
 package com.coderscampus.Week14.dto;
 
 public class Person {
+	private Long id;
 	private String name;
 	private String gender;
 	private Integer age;
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
@@ -24,7 +31,30 @@ public class Person {
 		this.age = age;
 	}
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
 	public String toString() {
-		return "Person [name=" + name + ", gender=" + gender + ", age=" + age + "]";
+		return "Person [id=" + id + ", name=" + name + ", gender=" + gender + ", age=" + age + "]";
 	}
 }
--- Week14/src/main/java/com/coderscampus/Week14/web/PersonController.java
+++ Week14/src/main/java/com/coderscampus/Week14/web/PersonController.java
@@ -1,21 +1,28 @@
 package com.coderscampus.Week14.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coderscampus.Week14.dto.Person;
+import com.coderscampus.Week14.repository.PersonRepository;
 
 @RestController
 public class PersonController {
 	
+	@Autowired
+	private PersonRepository personRepo;
+	
 	@PostMapping("/persons")
-	public Person createPerson (String name, String gender, Integer age) {
+	public Person createPerson (String name, String gender, Integer age, Long id) {
 		Person person = new Person();
 		person.setName(name);
 		person.setAge(age);
 		person.setGender(gender);
+		person.setId(id);
+		personRepo.save(person);
 		return person;
 	}
 	
@@ -25,7 +32,7 @@ public class PersonController {
 	@GetMapping("/persons/{personId}")
 	public Person getPerson (@PathVariable Long personId) {
 		System.out.println("Person ID is: " + personId);
-		return null;
+		return personRepo.findById(personId);
 	}
 	
 }

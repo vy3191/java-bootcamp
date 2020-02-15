8f1b7be0929e1f73f090c989af35a2cf04c686e4
Trevor Page <trevor@craftycodr.com>
2020-02-15 11:03:27 -0500

    03 - PostMapping with a Regular Controller

--- Week15/src/main/java/com/coderscampus/Week15/web/WelcomeController.java
+++ Week15/src/main/java/com/coderscampus/Week15/web/WelcomeController.java
@@ -3,6 +3,9 @@ package com.coderscampus.Week15.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.coderscampus.Week15.domain.Person;
 
 @Controller
 public class WelcomeController {
@@ -12,11 +15,16 @@ public class WelcomeController {
 		//   This is what gets returned / resolved when we
 		//     return a String inside of a regular Controller GetMapping method
 		//   /src/main/resources/templates/welcome.html
-		String firstName = "Trevor";
-		String lastName = "Page";
-		model.put("firstName", firstName);
-		model.put("lastName", lastName);
+		Person person = new Person();
 		
+		model.put("person", person);
+		
+		return "welcome";
+	}
+	
+	@PostMapping("")
+	public String postRootWebpage (Person person) {
+		System.out.println(person);
 		return "welcome";
 	}
 }

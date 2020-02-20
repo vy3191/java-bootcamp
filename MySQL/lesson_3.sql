4cb4ef205d28686ea0b41186b860efa8638c4240
Trevor Page <trevor@craftycodr.com>
2020-02-20 16:58:45 -0500

    03 - Creating a Database Table

---
+++ MySQL/lesson_3.sql
@@ -0,0 +1,9 @@
+-- create database online_bank;
+-- use online_bank;
+create table users (
+	user_id int, 
+    username varchar(100), 
+    password varchar(32), 
+    first_name varchar(50),
+    last_name varchar(50)
+);

9d855cdb3bc500209e6cd9b40b880f26d5f8bd31
Trevor Page <trevor@craftycodr.com>
2020-03-02 16:41:49 -0500

    06 - Filtering with Joins and using the IN keyword

---
+++ MySQL/lesson_16.sql
@@ -0,0 +1,13 @@
+select * from `users` u
+left join `address` a on u.user_id = a.user_id
+join `user_account` ua on ua.user_id = u.user_id
+join `account` acc on acc.account_id = ua.account_id
+join `transactions` t on t.account_id = acc.account_id
+where u.user_id = 1;
+
+select * from `users` u
+left join `address` a on u.user_id = a.user_id
+join `user_account` ua on ua.user_id = u.user_id
+join `account` acc on acc.account_id = ua.account_id
+join `transactions` t on t.account_id = acc.account_id
+where username in ('trevor@craftycodr.com', 'john@doe.com');

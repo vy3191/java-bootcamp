6150cd5ac42d5ff14f44e53f8951c6234090be0d
Trevor Page <trevor@craftycodr.com>
2020-03-06 10:02:24 -0500

    01 - Introduction to Hibernate and Spring Data JPA

---
+++ hibernate-example/src/main/java/com/coderscampus/week18/hibernateexample/HibernateExampleApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.week18.hibernateexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateExampleApplication.class, args);
+	}
+
+}

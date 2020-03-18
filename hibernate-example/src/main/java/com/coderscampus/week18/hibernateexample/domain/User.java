051b2cb6a469bbfdabf3ef3b522052615028cabe
Trevor Page <trevor@craftycodr.com>
2020-03-18 09:43:10 -0400

    10 - Eager vs Lazy Fetching

--- hibernate-example/src/main/java/com/coderscampus/week18/hibernateexample/domain/User.java
+++ hibernate-example/src/main/java/com/coderscampus/week18/hibernateexample/domain/User.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -57,7 +58,7 @@ public class User {
 	public void setCreatedDate(LocalDate createdDate) {
 		this.createdDate = createdDate;
 	}
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.LAZY)
 	@JoinTable(name = "user_account",
 	           joinColumns = @JoinColumn(name = "user_id"), 
 	           inverseJoinColumns = @JoinColumn(name = "account_id"))

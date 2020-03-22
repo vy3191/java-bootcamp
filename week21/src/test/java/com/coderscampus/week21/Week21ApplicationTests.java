459e5559ca195d6b2e1d7e0394e52bef8a038dce
Trevor Page <trevor@craftycodr.com>
2020-03-22 10:08:33 -0400

    01 - Setting up a new project

---
+++ week21/src/test/java/com/coderscampus/week21/Week21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.week21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

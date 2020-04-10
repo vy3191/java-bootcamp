630b7a8df38b64c288f151fbee09e3b49b2e7728
Trevor Page <trevor@craftycodr.com>
2020-04-10 12:13:21 -0400

    07 - QuerySelectorAll and FontAwesome

--- week22/src/main/resources/static/js/register.js
+++ week22/src/main/resources/static/js/register.js
@@ -1,4 +1,25 @@
 var usernameTextbox = document.querySelector('#username')
+var eyeIcons = document.querySelectorAll('.fa-eye')
+
+eyeIcons.forEach( (eyeIcon) => {
+	eyeIcon.addEventListener('click', () => {
+		if (eyeIcon.classList.contains('fa-eye')) {
+			eyeIcon.classList.replace('fa-eye', 'fa-eye-slash')
+			if (eyeIcon.getAttribute('id') === 'passwordEyeIcon') {
+				document.querySelector("#password").type = 'text'
+			} else {
+				document.querySelector("#confirmPassword").type = 'text'
+			}
+		} else {
+			eyeIcon.classList.replace('fa-eye-slash', 'fa-eye')
+			if (eyeIcon.getAttribute('id') === 'passwordEyeIcon') {
+				document.querySelector("#password").type = 'password'
+			} else {
+				document.querySelector("#confirmPassword").type = 'password'
+			}
+		}
+	})
+})
 
 // this is for demonstration purposes
 function myPromise () {

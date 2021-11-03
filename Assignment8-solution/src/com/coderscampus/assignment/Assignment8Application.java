ba7192b8519536b04eef673e8110ee868431fa6d
tp02ga <trevor@craftycodr.com>
2021-11-03 10:04:16 -0400

    found and fixed bug in assignment 8 code

--- Assignment8-solution/src/com/coderscampus/assignment/Assignment8Application.java
+++ Assignment8-solution/src/com/coderscampus/assignment/Assignment8Application.java
@@ -10,33 +10,34 @@ import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class Assignment8Application {
-	public static void main(String[] args) {
-		Assignment8 assignment8 = new Assignment8();
-		
-		List<Integer> allNumbers = Collections.synchronizedList(new ArrayList<>(1000));
-		
-		ExecutorService executor = Executors.newCachedThreadPool();
-		
-		List<CompletableFuture<Void>> tasks = new ArrayList<>(1000);
-		
-		for (int i=0; i<1000; i++) {
-			CompletableFuture<Void> task = 
-				CompletableFuture.supplyAsync(() -> assignment8.getNumbers(), executor)
-								 .thenAccept(numbers -> allNumbers.addAll(numbers));
-			tasks.add(task);
-		}
-		
-		while (tasks.stream().filter(CompletableFuture::isDone).count() < 1000 ) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-			}
-		}
-		
-		System.out.println("Done getting all 1,000,000 numbers");		
-		System.out.println("This number should be one million: " + allNumbers.size());
-		
-		// This is the "For Loop" way of doing things
+    public static void main(String[] args) {
+        Assignment8 assignment8 = new Assignment8();
+
+        List<Integer> allNumbers = Collections.synchronizedList(new ArrayList<>(1000));
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        List<CompletableFuture<Void>> tasks = new ArrayList<>(1000);
+
+        for (int i = 0; i < 1000; i++) {
+            CompletableFuture<Void> task = CompletableFuture.supplyAsync(() -> assignment8.getNumbers(), executor)
+                    .thenAccept(numbers -> allNumbers.addAll(numbers));
+            tasks.add(task);
+        }
+
+        while (tasks.stream().filter(CompletableFuture::isDone).count() < 1000)
+
+        {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+            }
+        }
+
+        System.out.println("Done getting all 1,000,000 numbers");
+        System.out.println("This number should be one million: " + allNumbers.size());
+
+        // This is the "For Loop" way of doing things
 //		Map<Integer, Integer> output = new HashMap<>();
 //		allNumbers.stream()
 //		          .forEach(number -> {
@@ -47,11 +48,11 @@ public class Assignment8Application {
 //		        	  }
 //		          });
 //		System.out.println(output);
-		
-		// this is the Stream Collector.toMap way of doing things
-		Map<Integer, Integer> output = allNumbers.stream()
-				  .collect(Collectors.toMap(i -> i, i -> 1, (oldValue, newValue) -> oldValue + 1));
-		System.out.println(output);
-		
-	}
+
+        // this is the Stream Collector.toMap way of doing things
+        Map<Integer, Integer> output = allNumbers.stream()
+                .collect(Collectors.toMap(i -> i, i -> 1, (oldValue, newValue) -> oldValue + 1));
+        System.out.println(output);
+
+    }
 }
